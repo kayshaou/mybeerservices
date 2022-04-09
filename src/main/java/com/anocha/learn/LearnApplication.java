51f100989f719637f0bdb8007b1396749b5dde0a
kayshaou <kaysha.ou@gmail.com>
2022-04-09 11:06:31 +0800

    add beerDto

--- src/main/java/com/anocha/learn/LearnApplication.java
+++ src/main/java/com/anocha/learn/LearnApplication.java
@@ -1,13 +1,24 @@
 package com.anocha.learn;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class LearnApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LearnApplication.class, args);
-	}
+    public static void main(String[] args) {
+        try {
+            System.setProperty("spring.devtools.restart.enabled", "false");
+            SpringApplication.run(LearnApplication.class, args);
+        } catch (Throwable e) {
+            if(e.getClass().getName().contains("SilentExitException")) {
+                log.debug("Spring is restarting the main thread - See spring-boot-devtools");
+            } else {
+                log.error("Application crashed!", e);
+            }
+        }
+    }
 
 }
---
+++ src/main/java/com/anocha/learn/model/BeerDto.java
@@ -0,0 +1,29 @@
+package com.anocha.learn.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class BeerDto {
+
+    private UUID id;
+    private Integer version;
+    private OffsetDateTime createdDatetime;
+    private OffsetDateTime lastModified;
+    private String beerName;
+    private BeerStyleEnum beerStyle;
+    private Long upc;
+    private BigDecimal price;
+    private Integer quantityOnHand;
+
+
+}
