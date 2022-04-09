cd480b58507c89bec4b52b6c8a4e1ecb8b2477a6
kayshaou <kaysha.ou@gmail.com>
2022-04-09 12:05:03 +0800

    added controller

---
+++ src/main/java/com/anocha/learn/controller/BeerController.java
@@ -0,0 +1,30 @@
+package com.anocha.learn.controller;
+
+
+import com.anocha.learn.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable UUID beerId) {
+        return new ResponseEntity<BeerDto>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<BeerDto> saveBeer(@RequestBody BeerDto beerDto) {
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity<BeerDto> updateBeerById(@PathVariable UUID beerId, @RequestBody BeerDto beerDto) {
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+}
