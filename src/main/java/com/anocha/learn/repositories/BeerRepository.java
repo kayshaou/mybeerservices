84d7acf4a6f4f4edb2aa0544dbca19ec71b4639a
kayshaou <kaysha.ou@gmail.com>
2022-04-09 21:03:13 +0800

    add Beer repo closes #6

---
+++ src/main/java/com/anocha/learn/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.anocha.learn.repositories;
+
+import com.anocha.learn.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
